733c640a1ebfe8e20f7e965a3a7387562ba5230d
Zane <z8872453@yahoo.com.tw>
2020-05-21 16:48:01 +0800

    SVM Grid Search Second Version

--- svm.py
+++ svm.py
@@ -3,6 +3,11 @@ import numpy as np
 from libsvm.svmutil import *
 from libsvm.svm import *
 
+X_train = extract_data('./X_train.csv')
+X_test = extract_data('./X_test.csv')
+Y_train = extract_data('./Y_train.csv').reshape(5000)
+Y_test = extract_data('./Y_test.csv').reshape(2500)
+
 def extract_data(file):
     csvfile = open(file)
     data = csv.reader(csvfile)
@@ -31,17 +36,16 @@ def grid_search(method):
 
     best_parameter = max(parameters)
     print (f'best score {best_parameter[0]}% for {method} with gamma {best_parameter[1]} and cost {best_parameter[2]}')
+
+    model = svm_train(Y_train, X_train, f'-t 2 -q -g {best_parameter[1]} -c {best_parameter[2]} -v 5')
+    predict = svm_predict(Y_test, X_test, model)
+
     return best_parameter
 
 if __name__ == "__main__":
-    X_train = extract_data('./X_train.csv')
-    X_test = extract_data('./X_test.csv')
-    Y_train = extract_data('./Y_train.csv').reshape(5000)
-    Y_test = extract_data('./Y_test.csv').reshape(2500)
-
     # refer to https://www.cnblogs.com/Finley/p/5329417.html
     # linear kernel functions
-    '''model_linear = svm_train(Y_train, X_train, '-t 0 -q')
+    model_linear = svm_train(Y_train, X_train, '-t 0 -q')
     predict_linear = svm_predict(Y_test, X_test, model_linear)
 
     # polynomial kernel functions
@@ -50,7 +54,7 @@ if __name__ == "__main__":
 
     # RBF kernel functions
     model_rbf = svm_train(Y_train, X_train, '-t 2 -q')
-    predict_rbf = svm_predict(Y_test, X_test, model_rbf)'''
+    predict_rbf = svm_predict(Y_test, X_test, model_rbf)
 
     # doing grid search
     grid_linear = grid_search('linear')
